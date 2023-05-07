b7dac38987a79cbfa87fca594feb6c926ce506a9
zigabencina <zigabencina1@gmail.com>
2023-05-07 21:14:46 +0200

    api :)

---
+++ Trading_Broker_Project/AVConnection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trading_Broker_Project
+{
+    internal class AVConnection
+    {
+       
+        private readonly string _apiKey;
+
+        public AVConnection(string apiKey)
+        {
+            this._apiKey = apiKey; // pri klicu objekta vnesemo kot parameter api kljuc
+        }
+
+        public void SaveCSVFromURL(string symbol)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://" + $@"www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={this._apiKey}&datatype=csv"); // pošljemo request strani
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse(); //vrne nam csv datoteko z podatki
+
+            StreamReader sr = new StreamReader(resp.GetResponseStream()); // uporabi streamreader iz knjižnice system.io da prebermo csv datoteko
+            string results = sr.ReadToEnd(); // zapisemo v spremelnjivko kot string
+            sr.Close();
+            File.WriteAllText("stockdata.csv", results); // zapisemo v lokalno datoteko
+        }
+
+    }
+}
---
+++ Trading_Broker_Project/Graph.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Trading_Broker_Project
+{
+    public partial class Graph : Form
+    {
+        public Graph()
+        {
+            InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public static void addToGraph()
+        {
+            AVConnection conn = new AVConnection("demo"); // definiramo objekt in damo v funkcijo api kljuc, ki smo ga dobili na spletni strani --> apikey = ZL4LHLXWTVKH4U4Q
+            conn.SaveCSVFromURL("IBM");// povemo za kero valuto bi radi dobili podatke
+
+            
+            string coloumn1;
+            string coloumn2;
+            var path = "stockdata.csv";
+            using (TextFieldParser csvReader = new TextFieldParser(path))
+            {
+                csvReader.CommentTokens = new string[] { "#" };
+                csvReader.SetDelimiters(new string[] { "," });
+                csvReader.HasFieldsEnclosedInQuotes = true;
+
+                // Skip the row with the column names
+                csvReader.ReadLine();
+
+                while (!csvReader.EndOfData)
+                {
+                    // Read current line fields, pointer moves to the next line.
+                    string[] fields = csvReader.ReadFields();
+                    coloumn1 = fields[0];
+                    coloumn2 = fields[1];
+                }
+
+            }
+        }
+    }
+}
